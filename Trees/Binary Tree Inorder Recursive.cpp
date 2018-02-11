24b738064a80c7fbeae6c2e2c3b3c630397636f8
Pranaya Tomar <pranayatomar@msn.com>
2018-02-11 17:20:36 +0530

    Create Binary Tree Inorder Recursive.cpp

---
+++ Trees/Binary Tree Inorder Recursive.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+class Node{
+    
+    public :
+            Node* left;
+            Node* right;
+            
+            int data;
+            
+            Node(int data){
+                this->left = NULL;
+                this->right = NULL;
+                this->data = data;
+            }
+       
+};
+
+void inorder(Node* root){
+    
+    if(root == NULL){ return;}
+    
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+    
+    return;
+}
+int main(){
+    //level 1
+   Node *root = new Node(1); 
+   
+   //level 2
+   root->left = new Node(2);
+   root->right = new Node(3);
+   
+   //level 3
+   root->left->left = new Node(4);
+   root->left->right = new Node(5);
+   root->right->left = new Node(6);
+   root->right->right = new Node(7);
+   
+   //level 4
+   root->left->right->left = new Node(8);
+   root->left->right->right = new Node(9);
+   
+   inorder(root);
+   
+    return 1;
+}
